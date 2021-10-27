c31a2762df6b5395cd521422094c65a56f40a91c
emalexander15 <emalexander@csumb.edu>
2021-10-26 17:00:39 -0700

    otter puns

--- js/site.js
+++ js/site.js
@@ -3,21 +3,49 @@
 // public domain
 
 // return answers
-// depending on length mod of 8
+// depending on length mod of 12
 
 function answerMonte(str) {
 	len = str.length;
-  mod = len % 3;
+  mod = len % 12;
   if (mod == 0) {
-  	return "Good outcome 1"
+  	return "I am otterly sure!"
   }
   else if (mod == 2) {
-  return "Bad outcome 1"
+  return "I'm not sure, you've left me in otter disbelief."
   }
   else if (mod == 3) {
-  return "Neutral outcome 1"
+  return "That's otter nonsense."
   }
+	else if (mod == 4) {
+  return "Yes, you're not like the otters!"
+  }
+  else if (mod == 5) {
+  return "Ask again later, I otter know better."
+  }
+	else if (mod == 6) {
+	return "No, get otter here."
+	}
+	else if (mod == 7) {
+	return "The way I sea it, I'm otterly certain.""
+	}
+	else if (mod == 8) {
+	return "I'm otterly unsure."
+	}
+	else if (mod == 9) {
+	return "You otter not count on it."
+	}
+	else if (mod == 10) {
+	return "My kelp beds point to yes." or "Outlook is otterly good."
+	}
+	else if (mod == 11) {
+	return "You otter have something better to do."
+	}
+	else if (mod == 12) {
+	return "Nah, you otter get some b*tches."
+	}
 }
+
 
 // click event
 var myButton = document.getElementById("submit");

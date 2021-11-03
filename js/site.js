5e8106e79dd95339955d5ab018ed936939fcba60
holyneotrin <tphanlow@csumb.edu>
2021-11-03 14:37:59 -0700

    fixed the background css, edited the js + html

--- js/site.js
+++ js/site.js
@@ -11,38 +11,41 @@ function answerMonte(str) {
   if (mod == 0) {
   	return "I am otterly sure!"
   }
-  else if (mod == 2) {
+  else if (mod == 1) {
   return "I'm not sure, you've left me in otter disbelief."
   }
-  else if (mod == 3) {
+  else if (mod == 2) {
   return "That's otter nonsense."
   }
-	else if (mod == 4) {
+	else if (mod == 3) {
   return "Yes, you're not like the otters!"
   }
-  else if (mod == 5) {
+  else if (mod == 4) {
   return "Ask again later, I otter know better."
   }
-	else if (mod == 6) {
+	else if (mod == 5) {
 	return "No, get otter here."
 	}
-	else if (mod == 7) {
-	return "The way I sea it, I'm otterly certain.""
+	else if (mod == 6) {
+	return "The way I sea it, I'm otterly certain."
 	}
-	else if (mod == 8) {
+	else if (mod == 7) {
 	return "I'm otterly unsure."
 	}
-	else if (mod == 9) {
+	else if (mod == 8) {
 	return "You otter not count on it."
 	}
-	else if (mod == 10) {
-	return "My kelp beds point to yes." or "Outlook is otterly good."
+	else if (mod == 9) {
+	return "My kelp beds point to yes."
 	}
-	else if (mod == 11) {
+	else if (mod == 10) {
 	return "You otter have something better to do."
 	}
-	else if (mod == 12) {
+	else if (mod == 11) {
 	return "Nah, you otter get some b*tches."
+	}
+	else if (mod == 12){
+		return "Outlook is otterly good."
 	}
 }
 
@@ -52,12 +55,8 @@ var myButton = document.getElementById("submit");
 myButton.addEventListener("click",function(){
   var question = document.getElementById("input").value;
   var answer = answerMonte(name);
-  //displays output
+//displays output
   newText = "<h3>Monte says: " + answer + ".</h3>";
   document.getElementById("output").innerHTML = newText;
   document.getElementById("answer").innerHTML = answer;
 })
-
-function myFunction() {
-  var x = document.getElementById("myAudio").autoplay;
-}

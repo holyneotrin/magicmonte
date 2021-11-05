694473bb10298bd17b5955289b8173d6c642248a
holyneotrin <tphanlow@csumb.edu>
2021-11-04 17:24:50 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -2,6 +2,7 @@
 // date: 10.26.2021
 // public domain
 
+// random value JS array is from -> https://stackoverflow.com/questions/4550505/getting-a-random-value-from-a-javascript-array
 const answers = [
   "I am otterly sure!",
   "I'm not sure, you've left me in otter disbelief.",
@@ -28,64 +29,3 @@ myButton.addEventListener("click",function(){
 //displays output
    document.getElementById("output").innerHTML = answer;
 })
-
-
-
-// // return answers
-// // depending on length mod of 12
-//
-// function answerMonte(str) {
-// 	len = str.length;
-//   mod = len % 13;
-//   if (mod == 0) {
-//   	return "I am otterly sure!"
-//   }
-//   else if (mod == 1) {
-//   	return "I'm not sure, you've left me in otter disbelief."
-//   }
-//   else if (mod == 2) {
-//   	return "That's otter nonsense."
-//   }
-// 	else if (mod == 3) {
-// 	  return "Yes, you're not like the otters!"
-//   }
-//   else if (mod == 4) {
-// 	  return "Ask again later, I otter know better."
-//   }
-// 	else if (mod == 5) {
-// 		return "No, get otter here."
-// 	}
-// 	else if (mod == 6) {
-// 		return "The way I sea it, I'm otterly certain."
-// 	}
-// 	else if (mod == 7) {
-// 		return "I'm otterly unsure."
-// 	}
-// 	else if (mod == 8) {
-// 		return "You otter not count on it."
-// 	}
-// 	else if (mod == 9) {
-// 			return "My kelp beds point to yes."
-// 	}
-// 	else if (mod == 10) {
-// 		return "You otter have something better to do."
-// 	}
-// 	else if (mod == 11) {
-// 		return "Nah, you otter get some b*tches."
-// 	}
-// 	else if (mod == 12) {
-// 		return "Outlook is otterly good."
-// 	}
-// }
-//
-//
-// // click event
-// var myButton = document.getElementById("submit");
-// myButton.addEventListener("click",function(){
-//   var question = document.getElementById("input").value;
-//   var answer = answerMonte(name);
-// //displays output
-//   newText = "<h3>Monte says: " + answer + "</h3>";
-//   document.getElementById("output").innerHTML = newText;
-//   document.getElementById("answer").innerHTML = answer;
-// })
